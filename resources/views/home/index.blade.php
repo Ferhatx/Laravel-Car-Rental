99da9bd4bcb120278898f403865814f5ddce8119
Ferhatx <ferhatakturk@outlook.com.tr>
2020-12-07 01:03:48 +0300

    index değişti

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,5 +2,11 @@
 
 @extends('layouts.home')
 
-@section('title', 'X RENT A CAR')
+@section('title', 'X Rent A Car')
 
+@section('description')
+    En Uygun Araçlar X Rent A Car'da.Güvenle Aracını Kullanın.Araçlarımız her kullanımdan sonra dezenfekte edilir.
+@endsection
+@section('keywords')
+    araç,kiralama,rent a car,Türkiyede Araç Kiralama
+@endsection
